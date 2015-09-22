2de2b5c9682b24df1c4b0f13680d21740e00b23a
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2015-09-22 23:52:45 +0800

    added models for variable CLASS

---
+++ FittingModelsClass.R
@@ -0,0 +1,48 @@
+# Clear Workspace
+rm(list = ls())
+
+# Necessary libraries
+library(caret)
+library(plyr)
+library(Metrics)
+library(mice)
+library(ggplot2)
+library(RColorBrewer)
+library(rms)
+
+
+
+# Set seed for reproducibility and also set working directory
+set.seed(1)
+setwd("C:/Users/User/Documents/GitHub/Cardiotocography")
+
+load(file = "data.rda")
+
+
+# Further partitioning our original training data into training and test sets
+inTrain         = createDataPartition(data$CLASS, p = 0.8)[[1]]
+training        <- data[inTrain,]
+remainder        <- data[-inTrain,]
+
+
+
+#### MODELS
+
+
+# Random Forest
+
+# Parameters for caret's train
+fitControl <- trainControl(method = "oob")
+
+rf <- train(CLASS~.,
+            data = training[-39], 
+            method = "rf",
+            ntree = 1000,
+            trControl = fitControl,
+            varImp = TRUE
+)
+
+prediction_rf <- predict(rf, newdata = remainder)
+confusionMatrix(remainder$CLASS, prediction_rf)
+
+save(rf, file = "rfClass.rda")
