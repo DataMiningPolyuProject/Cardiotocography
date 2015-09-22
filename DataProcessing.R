8c13393d434b3299db5cd499d47e3b0f744c101f
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2015-09-22 23:24:00 +0800

    added data processing and processed data

--- DataProcessing.R
+++ DataProcessing.R
@@ -2,25 +2,41 @@
 rm(list = ls())
 
 # Necessary libraries
-library(caret)
-library(plyr)
-library(Metrics)
-library(mice)
-library(ggplot2)
-library(RColorBrewer)
-library(rms)
-
-
+library(XLConnect)  
 
 # Set seed for reproducibility and also set working directory
 set.seed(1)
 setwd("C:/Users/User/Documents/GitHub/Cardiotocography")
 
-load(file="submission_data999.rda")
-load(file="train_data999.rda")
 
 
-# Further partitioning our original training data into training and test sets
-inTrain         = createDataPartition(train_data$cuisine, p = 0.8)[[1]]
-training        <- train_data[inTrain,]
-remainder        <- train_data[-inTrain,]
+
+wk = loadWorkbook("CTG.xls") 
+data = readWorksheet(wk, sheet="Data")
+
+# Column names are given in the first row of dataset. Let's use them.
+column_names <- as.character(data[1,])
+colnames(data) = column_names
+data  = data[-1,]
+
+# How many observations in the column are NA 
+na_columns_rate = sapply(data, FUN = function(x) sum(is.na(x))/length(x))
+
+# Remove almost fully NA columns
+data = data[,na_columns_rate < 0.4]
+
+# Remove NA rows. There are 3 of them
+data = data[complete.cases(data),]
+
+
+# Make all the columns numeric
+original_data_types = sapply(data, typeof)
+data <- data.frame(lapply(data, FUN = function(x) as.numeric(sub(",", ".", x, fixed = TRUE))))
+
+# Treat all columns with less than 20 unique values as factors
+unique_values_count = sapply(data, FUN = function(x) length(unique(x)))
+data[,unique_values_count<21] = data.frame(lapply(data[,unique_values_count<21], FUN = function(x) as.factor(as.character(x))))
+
+
+# saving
+save(data, file = "data.rda")
