939753efd71a9ac6d57d45cee4c55b35156fde09
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2015-09-25 00:13:22 +0800

    initial data visualisation

---
+++ DataVisualisation.R
@@ -0,0 +1,20 @@
+# Clear Workspace
+rm(list = ls())
+
+# Necessary libraries
+library(ggplot2)
+library(GGally)
+
+
+
+
+# Set seed for reproducibility and also set working directory
+set.seed(1)
+setwd("C:/Users/User/Documents/GitHub/Cardiotocography")
+
+load(file = "data.rda")
+
+
+png(filename = "pairs.png", width = 2000, height = 2000)
+ggpairs(data[,c(1,2,3,10,15,34,35)], colour='NSP')
+dev.off()
