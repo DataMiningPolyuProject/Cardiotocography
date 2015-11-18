e581f460fe9ad378f2c6c98c5cc29f8e2bb42a8d
Dmitrii Krasheninnikov <dmkr0001@gmail.com>
2015-11-18 22:32:35 +0800

    better plots

--- DataVisualisation.R
+++ DataVisualisation.R
@@ -9,33 +9,39 @@ library(lattice)
 
 # Set seed for reproducibility and also set working directory
 set.seed(1)
-<<<<<<< HEAD
+
 setwd("C:/Users/User/Documents/GitHub/Cardiotocography")
 load(file = "dat/raw_data.rda")
 
 
 
-#png(filename = "pairs.png", width = 2000, height = 2000)
+#png(filename = "pairs.png", width = 800, height = 600)
 #ggpairs(raw_data[,1:15], color= raw_data$NSP)
 #dev.off()
 
 
-
-ggplot(raw_data, aes(x = Width, y = Min, colour = NSP)) + geom_point(size = 3) + scale_colour_brewer(palette = 11) + theme(panel.background = element_rect(fill = "gray42"))
-
-
-=======
-load(file = "dat/raw_data.rda")
->>>>>>> origin/master
-
-
-png(filename = "AC_hist.png", width = 800, height = 600)
-plot1 = qplot(AC, data=raw_data, geom="histogram", color = NSP, fill=I("gray"))
-plot1 +  scale_colour_brewer(palette = 1) 
+png(filename = "width_vs_min.png", width = 800, height = 600)
+width_vs_min <- ggplot(raw_data, aes(x = Width, y = Min, colour = NSP)) +
+        geom_point(size = 3) + scale_colour_brewer(palette = 11) +
+        theme(panel.background = element_rect(fill = "gray42"))
+width_vs_min
 dev.off()
 
 
-ggplot(raw_data, aes(x=AC, fill=NSP)) +
-        geom_histogram(position="dodge") +
-        scale_fill_brewer(palette = cbPalette)
 
+
+#png(filename = "AC_hist.png", width = 800, height = 600)
+#ac_hist <- qplot(AC, data=raw_data, geom="histogram", color = NSP, fill=I("white"))+
+#        scale_colour_brewer(palette = 11) +
+#        theme(panel.background = element_rect(fill = "gray42"))
+#ac_hist
+#dev.off()
+
+
+png(filename = "AC_hist.png", width = 800, height = 600)
+ac_hist <- ggplot(raw_data, aes(x=AC, fill=NSP)) +
+        geom_histogram(position="dodge") +
+        scale_fill_brewer(palette = 11) +
+        theme(panel.background = element_rect(fill = "gray42"))
+ac_hist
+dev.off()
